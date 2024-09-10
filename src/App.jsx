f11165ba40bdaad8e637f376a469a8eb4a3d5053
Duncan Ramuhashi <duncanramohashi@gmail.com>
2024-09-10 10:14:06 +0200

    lolo

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-
+import hero from './componets/hero'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
-      <p className="text-8xl">
-        Click on the Vite and React logos to learn more
-      </p>
+     <hero/>
+      
     </>
   )
 }
---
+++ src/componets/hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import navbar from './navbar'
+const hero = () => {
+  return (
+    <div>
+           <navbar/>
+
+    </div>
+  )
+}
+
+export default hero
