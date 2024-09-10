37207e718e653eba9252911c33f3b7c1e007f11d
Duncan Ramuhashi <duncanramohashi@gmail.com>
2024-09-10 09:57:34 +0200

    Start

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <p className="text-8xl">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
